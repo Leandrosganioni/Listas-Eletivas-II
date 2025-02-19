fd9e19387422976b1bf551942e9ebdc1d8d5ee09
Leandro Sganioni <leandrosgani@gmail.com>
2025-02-19 00:05:24 -0300

    Realizado primeiro exercício

--- primeiralista/routes/web.php
+++ primeiralista/routes/web.php
@@ -15,10 +15,21 @@ Route::get('/exer1', function(){
     return View('exer1');
 });
 
-Route::post('exer1resp', function(Request $request){
+Route::post('/exer1resp', function(Request $request){
     $valor1 = intval($request->input('valor1'));
     $valor2 = intval($request->input('valor2'));
     $soma = $valor1 + $valor2;
     return view('exer1', compact('soma'));
 });
 
+Route::get('/ex1', function(){
+    return view('lista.ex1');
+});
+
+Route::post('listaex1', function(Request $request){
+    $nota1 = floatval($request -> input('nota1'));
+    $nota2 = floatval($request -> input('nota2'));
+    $nota3 = floatval($request -> input('nota2'));
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    return view('lista.ex1', compact('media'));
+});
