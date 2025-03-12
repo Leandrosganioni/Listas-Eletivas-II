c4ce34b0c3c300972d3b48b75af2726d998439ca
Leandro Sganioni <leandrosgani@gmail.com>
2025-03-11 22:11:37 -0300

    adicionado projetoBanco

---
+++ projetobanco/database/migrations/2025_03_12_001853_create_clientes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id(); //PRIMARY KEY AUTO_INCREMENT
+            $table->string('nome'); //VANCHAR(100)
+            $table->string('email')->unique();
+            $table->decimal('salario', 8, 2);
+            $table->string('telefone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
